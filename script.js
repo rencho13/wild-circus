5a1fef6af7bd1320d4fb50d5379b090748f04d73
Rencho13 <stephone13@gmail.com>
2019-07-07 22:37:16 +0200

    integration du header responsive

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+		$('.menu-item').on('click', function() { // Au clic sur un élément
 			var page = $(this).attr('href'); // Page cible
 			var speed = 750; // Durée de l'animation (en ms)
 			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
