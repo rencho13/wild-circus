ce7edc3f52470edb3304db5bacc1e3f848beebb7
rencho13 <stephone13@gmail.com>
2019-07-08 12:42:07 +0200

    MAJ performance Responsive pas ok

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-		$('.menu-item').on('click', function() { // Au clic sur un élément
+		$('.menu-item, #cRetour').on('click', function() { // Au clic sur un élément
 			var page = $(this).attr('href'); // Page cible
 			var speed = 750; // Durée de l'animation (en ms)
 			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
 			return false;
 		});
 	});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.onscroll = function(ev) {
+    document.getElementById("cRetour").className = (window.pageYOffset > 250) ? "cVisible" : "cInvisible";
+  };
+});
